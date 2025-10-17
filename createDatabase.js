d3bca515cad8163c9a6aeec7ba8fb9cb8496d6b1
Axel Ravinale <a.ravinale@itec.riocuarto.org.ar>
2025-10-16 22:03:55 -0300

    express

--- createDatabase.js
+++ createDatabase.js
@@ -1,20 +1,22 @@
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2/promise');
 
 const crearBaseDeDatos = async () => {
     try {
         const connection = await mysql.createConnection({
             host: 'localhost',
             user: 'root',
-            password: 'root1234'
-        })
+            password: ''
+        });
 
-        await connection.query('CREATE DATABASE IF NOT EXISTS ')
-        console.log("base de datos creada exitosamente")
-        await connection.end()
+        await connection.query('CREATE DATABASE IF NOT EXISTS efijs CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;');
+        console.log("✅ Base de datos 'efijs' creada exitosamente");
+
+        await connection.end();
     } catch (error) {
-        console.error("Error creando la base de datos", error.message)
+        console.error("❌ Error creando la base de datos:", error.message);
     }
-}
+};
 
-crearBaseDeDatos()
+crearBaseDeDatos();
+
 
